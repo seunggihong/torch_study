3c1b888ec6b065cac13e70c2341be24d45613c61
seunggi <hsk1533@naver.com>
2024-06-30 15:47:12 +0900

    add

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import argparse
 from models.CNN.VGGnet import VGG
 
+def get_parser():
+    parser = argparse.ArgumentParser(add_help=False)
+    parser.add_argument('--epoch', type=int, default=3)
+    parser.add_argument('--net', type=str, default='VGG')
+    return parser
+
+def training(args):
+    for epoch in range(args.epoch):
+        print(epoch)
+
 if __name__ == '__main__':
-    model = VGG()
-    print(model)
+    parser = argparse.ArgumentParser('Pytorch model training', parents=[get_parser()])
+    args = parser.parse_args()
+    print(args)
+
+    if args.net == 'VGG':
+        model = VGG()
+        training(args)
